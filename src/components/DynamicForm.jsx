39f3e4dfb9984481e7b7994e11564cf5d1bdf714
Yurdoy <yurdoy@gmail.com>
2025-06-01 11:03:38 +0200

    Component DynamicForm.jsx and DynamicForm.module.css created

---
+++ src/components/DynamicForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useForm } from "react";
+import cls from "../components/DynamicForm.module.css";
+
+const DynamicForm = () => {
+  return <div></div>;
+};
+
+export default DynamycForm;
