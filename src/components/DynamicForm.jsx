1ffeb4e73cc99e5300990ce5a622fdac3acd5c6e
Yurdoy <yurdoy@gmail.com>
2025-06-02 12:03:01 +0200

    DynamicForm.jsx completed - useForm used to manage state and form validation. onSubmit function created to handle form send. 'watch' used to watch value of the field. Condition for showing second field is set - it will appeared when any letters will be entered in first field. Style for whole form, inputs, error messages created.

--- src/components/DynamicForm.jsx
+++ src/components/DynamicForm.jsx
@@ -1,9 +1,59 @@
 import React from "react";
-import { useForm } from "react";
+import { useForm } from "react-hook-form";
 import cls from "../components/DynamicForm.module.css";
 
 const DynamicForm = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const firstFieldValue = watch("firstField");
+
+  const onSubmit = (data) => {
+    console.log("Data submitted: ", data);
+  };
+  return (
+    <form className={cls.form} onSubmit={handleSubmit(onSubmit)}>
+      <div className={cls.form_firstField}>
+        <label htmlFor="firstField">First Field</label>
+        <input
+          id="firstField"
+          {...register("firstField", {
+            required: "This field is compulsory",
+            pattern: {
+              value: /[A-Za-z]+$/i,
+              message: "Please use latin letters only",
+            },
+          })}
+        />
+        {errors.firstField && <p>{errors.firstField.message}</p>}
+      </div>
+
+      {firstFieldValue !== "" && (
+        <div className={cls.form_secondField}>
+          <label htmlFor="secondField">Second Field</label>
+          <input
+            id="secondField"
+            {...register("secondField", {
+              required: "This field is compulsory",
+              pattern: {
+                value: /[A-Za-z]+$/i,
+                message: "Please use latin letters only",
+              },
+            })}
+          />
+          {errors.secondField && <p>{errors.secondField.message}</p>}
+        </div>
+      )}
+
+      <button className={cls.form_btn} type="submit">
+        Submit
+      </button>
+    </form>
+  );
 };
 
-export default DynamycForm;
+export default DynamicForm;
